e0557e534a20393bbf51612322562a5b965fa8eb
Rachana Kaparthi <140998470+Rachana-Kaparthi@users.noreply.github.com>
2024-10-27 16:19:13 +0530

    Update wt_st_SA.py

--- Codes/Dataset Generation/wt_st_SA.py
+++ Codes/Dataset Generation/wt_st_SA.py
@@ -42,30 +42,3 @@ def matrix_mul_test(A,B,pos):
     #             C[i][j] += PE_approx(ipA[i][k], B[k][j], pos)
     return C
 
-
-# A=np.random.randint(0,15, size=(3,3))
-# print("\n \n matrix A :")
-# for i in range(3):
-#     print(A[i])
-
-# B=np.random.randint(0,15, size=(3,3))
-# print("\n \n matrix B :")
-# for i in range(3):
-#     print(B[i])
-# print( "exact",matrix_mul_exact(A,B))
-# print( "approx",matrix_mul_approx(A,B,[1,4,5,6,2,3]))
-
-# # pos=[[7,7,7],[7,7,8],[6,6,6]]
-# op_mat=wt_st_sa_3x3_exact(matA,matB)
-# print("\n \n Final output matrix :")
-# for i in range(3):
-#     print(op_mat[i])
-
-# # matrix = [[0 for i in range(3)] for j in range(3)]
-# result = np.dot(matA,matB)
-# print("\n \n exact output matrix :")
-# for i in range(3):
-#     print(result[i])
-
-# # if op_mat.all()==result.all():
-# #     print("matrices match")
